e962f59f8772242bec2f25d4f3c3afa91bf5b9f3
María Marta Ramírez Gil <ram21342@uvg.edu.gt>
2024-08-23 20:02:56 -0600

    Login implemented no GUI

--- src/GUI/client.py
+++ src/GUI/client.py
@@ -13,7 +13,7 @@ class BasicClient(ClientXMPP):
     def session_start(self, event):
         self.send_presence()
         self.get_roster()
-        print("Conexión establecida y presencia enviada")
+        print("CONNECTED")
         asyncio.create_task(self.handle_send_message())
 
     async def handle_send_message(self):
@@ -34,17 +34,32 @@ class BasicClient(ClientXMPP):
         self.send_message(mto=mto, mbody=mbody, mtype='chat')
 
 if __name__ == '__main__':
-    # Configurar la política de asyncio para Windows
+    # Establecer política de eventos de asyncio para compatibilidad con Windows
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
-    # Configuración de logging
+    # Inicializar configuración de logging
     logging.basicConfig(level=logging.INFO)
 
-    # Crear instancia del cliente
-    xmpp = BasicClient('ram21342-1@alumchat.lol', '21342')
-    
-    # Conectar al servidor XMPP
-    xmpp.connect(disable_starttls=True, use_ssl=False)
+    # Mostrar menú principal
+    print("\n--- Bienvenido a XMPP Chat ---\n")
+    option_selected = ""
 
-    # Procesar eventos (sin bloqueo)
-    xmpp.process(forever=False)
+    while option_selected != "3":
+        print("\nOpciones del Menú Principal:\n1. Registrarse\n2. Iniciar sesión\n3. Salir\n")
+        option_selected = input("Seleccione una opción para continuar: ")
+
+        if option_selected == "1":
+            print("\nLa función de registro aun no está disponible en esta versión del cliente.\n")
+
+        elif option_selected == "2":
+            jid = input("\nPor favor, ingrese su JID: ")
+            password = input("Ingrese su contraseña: ")
+            xmpp = BasicClient(jid, password)
+            xmpp.connect(disable_starttls=True, use_ssl=False)
+            xmpp.process(forever=False)
+
+        elif option_selected == "3":
+            print("\nSaliendo\n")
+
+        else:
+            print("\nOpción no válida. Ingrese una opcion del 1 al 3.\n")
